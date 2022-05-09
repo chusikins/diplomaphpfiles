a5ba6b0bee42a800a525c41e29f2cda321ef5cc5
Dmitry Krivoshey <chusik7@gmail.com>
2022-05-09 20:35:26 +0300

    The NIR files
    Orig NIR testing files.
    Need to rework the links

---
+++ scripts/teacher_student_test/show_test.php
@@ -0,0 +1,148 @@
+<?php
+require '../db/database_connection.php';
+
+//вводим нужные переменные из таблиц.
+$ex_id = $_REQUEST['ex_id'];
+$select_query = "SELECT * FROM exercise WHERE ex_id = " . $ex_id;
+$result = mysqli_query($link, $select_query);
+if ($result){
+  $row = mysqli_fetch_array($result);
+  $exercise = $row['exercise_text'];
+  $sub_answers_amount = $row['sub_answers_amount'];
+  $image_amount = $row['image_amount'];
+  $sub_answers = array();
+  $sub_answer_text = array();
+  $image_data = array();
+  if ($sub_answers_amount != 0) {
+   for ($i=1; $i<=$sub_answers_amount; $i++){
+     $answer_query = "SELECT * FROM sub_answers WHERE question_id = ". $ex_id . " AND sub_answer_id = ". $i;
+     $ans_result = mysqli_query($link, $answer_query);
+     $ans_row = mysqli_fetch_array($ans_result);
+     $sub_answers[$i-1] = $ans_row['sub_answer'];
+     $sub_answer_text[$i-1] = $ans_row['sub_answers_text'];
+      }
+    }
+  //  $answer_query = "SELECT * FROM sub_answers WHERE question_id = ". $ex_id;
+  //  $ans_result = mysqli_query($link, $answer_query);
+  // if ($ans_result){
+  //   while($row2 = mysqli_fetch_array($ans_result)){
+  //     echo $row2[1]." ";
+  //     echo $row2[2]." ";
+  //   }
+  if ($image_amount != 0){
+    for($i=0; $i<$image_amount; $i++){
+      $image_query = "SELECT * FROM images WHERE ex_id = ". $ex_id . " AND pic_ex_id = ". $i;
+      $image_result = mysqli_query($link, $image_query);
+      $image_row = mysqli_fetch_array($image_result);
+      $image_data[$i] = $image_row['imagedata'];
+      $image_data[$i] = base64_encode($image_data[$i]);
+    }
+  }
+  //   array_push($answers, $row['answer_'.$i+1])
+}else{
+  die("Ошибка обнаружения задания с ID {$ex_id}");
+}
+
+ ?>
+<html lang="en" dir="ltr">
+  <head>
+    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.css">
+    <link rel="stylesheet" href="css/texmath.css">
+    <script src="https://cdn.jsdelivr.net/npm/markdown-it/dist/markdown-it.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/katex/dist/katex.min.js"></script>
+    <script src="texmath.js"></script>
+    <script type="text/javascript">
+    //так же скрипты для создания ячеек для ответа студента
+    var images = <?php echo json_encode($image_data); ?>;
+    var answers = <?php echo json_encode($sub_answers); ?>;
+    var sub_text = <?php echo json_encode($sub_answer_text); ?>;
+      function showImages(){
+         var number = images.length;
+         var container2 = document.getElementById("container2");
+         while (container2.hasChildNodes()) {
+             container2.removeChild(container2.lastChild);
+         }
+         for (i=0; i<number; i++){
+           var image = new Image();
+           image.src = 'data:image/jpeg;base64,'+ images[i];
+           container2.appendChild(image);
+         }
+      };
+      function addFields(){
+          // Number of inputs to create
+          var number = answers.length;
+          // Container <div> where dynamic content will be placed
+          var container = document.getElementById("container");
+          // Clear previous contents of the container
+          while (container.hasChildNodes()) {
+              container.removeChild(container.lastChild);
+          }
+          for (i=0;i<number;i++){
+              // Append a node with a random text
+              container.appendChild(document.createTextNode("Answer " + (i+1)));
+              container.appendChild(document.createElement("br"))
+              container.appendChild(document.createTextNode(sub_text[i] + "      "));
+              // Create an <input> element, set its type and name attributes
+              var input = document.createElement("input");
+              input.type = "text";
+              input.name = "member" + i;
+              input.id = "member" + i;
+              container.appendChild(input);
+              // Append a line break
+              container.appendChild(document.createElement("br"));
+          }
+      }
+      // простая функция для сравнения строки. НЕ финальное решение задачи
+      function checkfunc(){
+        var number = answers.length;
+        var div = document.getElementById('checker')
+        while (div.hasChildNodes()){
+          div.removeChild(div.lastChild);
+        }
+        for (i=0;i<number;i++){
+          var student_answer = document.getElementById("member"+i).value;
+          if (student_answer === answers[i]){
+            div.innerHTML += i+1 + " is correct <br>";
+          } else {
+              div.innerHTML += i+1 + " is incorrect <br>";
+          }
+        }
+      }
+
+    </script>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body onload="showImages()">
+    <div id="header"><h1>PHP & MySQL</h1></div>
+    <div id="example"></div>
+    <fieldset>
+      <div id="content">
+        <div id="out" class="markdown-body">
+
+        </div>
+        <div id="container2">
+
+        </div>
+    </fieldset>
+      <div id="container">
+        <a href="#" id="filldetails" onclick="addFields()">Вывести</a>
+      </div>
+      <button type="button" onclick="checkfunc()">Проверить</button>
+      <br>
+      <div id="checker">
+      </div>
+    </div>
+
+    <script>
+      const str = <?php echo json_encode($exercise); ?>;
+      document.addEventListener("DOMContentLoaded", () => {
+        const md = markdownit({html:true})
+                      .use(texmath, { engine: katex,
+                                      delimiters: 'dollars',
+                                      katexOptions: { macros: {"\\RR": "\\mathbb{R}"} } } );
+        out.innerHTML = md.render(str);
+    })
+    </script>
+  </body>
+</html>
---
+++ teacher_student_test/test.php
@@ -0,0 +1,4 @@
+<?php
+require_once '../scripts/db/database_connection.php';
+echo $link;
+ ?>
